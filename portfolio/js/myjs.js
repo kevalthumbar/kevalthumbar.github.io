050c20c6e0e91316b2eb04194fee544a47922545
kevalthumbar <76449877+kevalthumbar@users.noreply.github.com>
2024-12-15 13:00:51 +0530

    improve animations

--- portfolio/js/myjs.js
+++ portfolio/js/myjs.js
@@ -1,7 +1,7 @@
 // Optimize intersection observer code for languages and skills sections
 
 const createObserver = (sectionSelector) => {
-    const section = document.querySelector(sectionSelector);
+    const section = document.querySelector(sectionSelector); //languages
     const elements = section.querySelectorAll(".progress-element");
 
     const observer = new IntersectionObserver((entries) => {
@@ -12,16 +12,25 @@ const createObserver = (sectionSelector) => {
                 const label = element.querySelector(".progress-label");
                 
                 if (entry.isIntersecting) {
-                    progressTag.classList.add("loading");
+                    // progressTag.classList.add("loading");
+
+                    let p = 0;
+                    const progressBar = setInterval(() => {
+                        progressTag.value = `${p}`
+                        if (p >= progressTag.dataset.progress) {
+                            clearInterval(progressBar);
+                        }
+                        p++;
+                    },15);
 
                     let i = 0;
                     const increment = setInterval(() => {
                         label.dataset.value = `${i}%`;
-                        if (i >= progressTag.value) {
+                        if (i >= progressTag.dataset.progress) {
                             clearInterval(increment);
                         }
                         i++;
-                    }, 10);
+                    }, 15);
                     
                 } else {
                     progressTag.classList.remove("loading");
